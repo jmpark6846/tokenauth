c0adc28f3a04d07863304fef866d0b8c14a08440
jmpark6846 <jmpark6846@gmail.com>
2018-11-19 10:53:38 +0900

    email로 토큰 발행

--- tokenauth/backends.py
+++ tokenauth/backends.py
@@ -1,26 +1,35 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
+from django.contrib.auth.models import User
+
 User = get_user_model()
 
 class EmailAuthenticationBackend:
     def authenticate(self, request, email=None, password=None):
+        '''
+        django default User 모델은 username이 pk이므로
+        username 필드에 email을 저장하거나 email이 pk인 custom user 모델을 만들어야한다.
+        여기선 전자를 선택.
+        :return: 인증 성공 시 유저 인스턴스, 실패 시 None
+        '''
+
         if email and password:
             try:
-                user = User.objects.get(email=email)
-            except:
+                user = User.objects.get(username=email)
+            except User.DoesNotExist:
                 return None
 
             password_valid = check_password(password, user.password)
 
             if password_valid:
                 return user
-            else:
-                return None
+
+            return None
         else:
             return None
 
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
-        except:
+        except User.DoesNotExist:
             return None
--- tokenauth/views.py
+++ tokenauth/views.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -8,8 +9,15 @@ class IssueAuthToken(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except serializers.ValidationError:
+            return Response({'success':False, 'message': '로그인에 실패했습니다. 아이디와 비밀번호를 확인하세요.'})
 
         user = serializer.validated_data['user']
+
+
         token, created = Token.objects.get_or_create(user=user)
+        print(token, created)
         return Response({ 'success': True, 'message':'성공', 'token:':token.key })
